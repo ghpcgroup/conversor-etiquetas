b5c2050cd234938ff11506b8f7b7564d445a6bac
GHPC GROUP <tighpc@gmail.com>
2026-04-15 11:01:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Configuração da página
-st.set_page_config(page_title="ZPL Converter | Empresa", page_icon="🏷️", layout="wide")
+st.set_page_config(page_title="Conversor GHPC", page_icon="🏷️", layout="wide")
 
 # --- BARRA LATERAL (CONFIGURAÇÕES) ---
 with st.sidebar:
